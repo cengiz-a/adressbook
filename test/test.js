7d86293ff08e0b0ae6dc60243b98181bf53a971c
cengiz-a <a.c.cengizhan@me.com>
2017-01-20 14:33:18 +0100

    karma start correctly

---
+++ test/test.js
@@ -0,0 +1,11 @@
+import mn from 'backbone.marionette';
+
+
+describe('Der Test für den Test', function () {
+    var a;
+    it('läuft', function () {
+        a = true;
+        expect(a).toBe(true);
+
+    })
+})
